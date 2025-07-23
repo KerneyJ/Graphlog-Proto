17c1bb62c2f00a8ce41bf64ea4aeecc3a80d594f
kerneyJ <gamerguy9801@gmail.com>
2025-07-23 17:30:50 -0500

    Changed interface to look more like kubectl
    instead of interactive select menu, the binary accepts 3 options.
    as of this commit the business logic for
    implementing those options has not been
    written. Next commit will contain that.

--- src/types/common.rs
+++ src/types/common.rs
@@ -1,3 +1,6 @@
+use serde::{Deserialize, Serialize};
+use clap::ValueEnum;
+
 pub type Id = Vec<u8>;
 pub type Key = (String, String);
 pub type Sig = Vec<u8>;
@@ -11,14 +14,20 @@ pub trait Decodable<T> {
 }
 
 // AT => ANCHOR_TYPE
-pub static AT_DNS: &str = "DNS";
-pub static AT_EMAIL: &str = "EMAIL";
-pub static AT_PHONE: &str = "PHONE";
-pub static AT_IPADDR: &str = "IP-ADDR";
 
-// CLMT => CLAIM_TYPE
-pub static CLMT_SSHKEY: &str = "SSHKEY";
-pub static CLMT_X509: &str = "X509";
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+pub enum AnchorType {
+    DNS,
+    EMAIL,
+    PHONE,
+    IPADDR,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+pub enum ClaimType {
+    SSHKEY,
+    X509,
+}
 
 // KT => KEY_TYPE
 pub static KT_ED25519: &str = "ED25519";
@@ -34,4 +43,24 @@ pub fn id_equal(id1: Id, id2: Id) -> bool {
         }
         true
     }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Config {
+    #[serde(rename = "client")]
+    pub client_conf: Option<ClientConfig>,
+    #[serde(rename = "server")]
+    pub server_conf: Option<ServerConfig>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct ClientConfig {
+    pub log_addr: String,
+    pub compiler_addr: Option<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct ServerConfig {
+    pub addr: String,
+    pub persist_path: Option<String>,
 }
